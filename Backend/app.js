20556d9c9829173e03fb548f12d487cf9c1babff
Nirmal-Kharal <nirmalkharal40@gmail.com>
2025-08-03 07:13:02 +0545

    connected mongo db

--- Backend/app.js
+++ Backend/app.js
@@ -1,12 +1,21 @@
 import express from 'express';
 import { nanoid } from 'nanoid';
+import dotenv from 'dotenv';
+dotenv.config({ path: "./.env" });
+import connectDB from './src/config/mongo.config.js';
 
 const app = express();
+app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Hello, World!');});
+
+app.use(express.urlencoded({ extended: true }));
+app.post('/', (req, res) => {
+  const {url}  = req.body;
+  console.log(url);
+  res.send(nanoid(7));});
 
 
 app.listen(3000, () => {
+  connectDB();
   console.log('Server is running on port 3000');
 });
